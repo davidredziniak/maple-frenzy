999b03ba8dc96f34a73329930cc492e50a27a0b1
davidredziniak <redziniakdavid@gmail.com>
2024-03-21 12:19:59 -0400

    Modify GET user information endpoint to provide profile data

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,18 +1,26 @@
 const db = require("../models");
 const User = db.users;
+const UserProfile = db.userProfiles;
 
 // Find user by ID
 exports.findById = (req, res) => {
   return User.findOne({ where: { id: req.params.userId } })
     .then((user) => {
-      if (!user) res.status(404).send({ error: "User not found." });
-      else
-        res.status(200).send({
+      if (!user) return res.status(404).send({ error: "User not found." });
+
+      // Get user profile (trade count and reputation)
+      UserProfile.findOne({ where: { userId: req.params.userId }}).then((userProfile) => {
+        if(!userProfile) return res.status(404).send({ error: "User profile not found." });
+        
+        return res.status(200).send({
           id: user.id,
           username: user.username,
+          tradeCount: userProfile.tradeCount,
+          reputation: userProfile.reputation,
           createdAt: user.createdAt,
           lastLoggedIn: user.lastLoggedIn,
         });
+      }).catch((error) => res.status(400).send(error));
     })
     .catch((error) => res.status(400).send(error));
 };

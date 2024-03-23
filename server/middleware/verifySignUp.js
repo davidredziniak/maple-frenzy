fea5cb83dced850493e65e880dc746a4bb41774c
davidredziniak <redziniakdavid@gmail.com>
2024-03-23 10:26:31 -0400

    Make sign up error codes more consistent

--- server/middleware/verifySignUp.js
+++ server/middleware/verifySignUp.js
@@ -3,7 +3,7 @@ const User = require("../models").users;
 // Checks if the username in the given request already exists
 const checkUsernameTaken = (req, res, next) => {
   User.findOne({ where: { username: req.body.username } }).then((user) => {
-    if (user) return res.status(400).send({ error: "Username already taken." });
+    if (user) return res.status(401).send({ message: "Username already taken." });
     next();
   });
 };

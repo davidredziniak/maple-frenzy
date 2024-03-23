5b71e8b074d8c3bfbe46981076eee794d0d78d79
sdk6 <sdk6@njit.edu>
2024-03-23 00:48:40 -0400

    test

--- ui/src/Register.js
+++ ui/src/Register.js
@@ -1,4 +1,4 @@
-import {stackLeft, stackRight, loginBox, loginText,gButtBox,gButtContent, signInButton } from './config'
+import {stackLeft, stackRight, loginBox, loginText, signInButton } from './config'
 import {
   ChakraProvider,
   Stack,
@@ -6,19 +6,29 @@ import {
   Flex,
   Text,
   Heading,
-  Link,
   Input,
   Button,
-  Center,
-  Box,
+  Box
 } from '@chakra-ui/react'
 import {
     FormControl,
     FormLabel,
-    FormErrorMessage,
-    FormHelperText,
   } from '@chakra-ui/react'
 import maplefrenzylogo from './maplefrenzylogo.svg'
+import {useState} from 'react'
+
+async function registerUser(username, password) {
+  return fetch('https://maple-frenzy.onrender.com/api/signup/', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: { username: (username), password: (password) }
+  })
+    .then(response => response.json())
+}
+
+
 const Backdrop = () =>{
     return(
       <Flex color="white" h="100vh">
@@ -27,23 +37,84 @@ const Backdrop = () =>{
           <Image src={maplefrenzylogo} />
         </Stack>
         <Stack {...stackRight} />
-        <LoginForm/>
+        <RegisterForm/>
       </Flex>
     )
   }
+
+const RegistrationForm = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const [errors, setErrors] = useState({});
   
-const LoginForm = () => {
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      const errors = {};
+
+      if (Object.keys(errors).length === 0) {
+        // Submit form data
+        console.log('Form submitted!', { username, password });
+        registerUser('myusername', 'mypassword')
+/*
+          .then(data => {
+            console.log('Registration successful:', data);
+            // Handle successful registration response
+          })
+          .catch(error => {
+            console.error('Registration failed:', error);
+            // Handle registration error
+          });
+*/
+      } else {
+        console.log('errors');
+        setErrors(errors);
+      }
+    };
+  
+    return (
+      <FormControl>
+        <div>
+          <FormLabel htmlFor="username">Username:</FormLabel>
+          <Input
+            type="username"
+            id="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+        <div>
+          <FormLabel htmlFor="password">Password:</FormLabel>
+          <Input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          {errors.password && <p>{errors.password}</p>}
+        </div>
+        <div>
+          <FormLabel htmlFor="confirmPassword">Confirm Password:</FormLabel>
+          <Input
+            type="password"
+            id="confirmPassword"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          />
+           {errors.confirmPassword && <p>{errors.confirmPassword}</p>}
+        </div>
+        <Button mt='20px' {...signInButton} type="submit" onClick={handleSubmit}>Register</Button>
+      </FormControl>
+    );
+  };
+  
+
+const RegisterForm = () => {
     return(
       <Stack {...loginBox}>
-        <Text {...loginText}>Register</Text>
-        <FormControl>
-            <FormLabel pt="20px">Email address</FormLabel>
-            <Input pr="20px" type='email' />
-            <FormLabel pt="30px">Password</FormLabel>
-            <Input type='password' />
-            <FormLabel pt="30px">Confirm Password</FormLabel>
-            <Input type='password' />
-        </FormControl>
+        <Text {...loginText}>User Registration</Text>
+        <RegistrationForm/>
+        <Box justifyContent="bottom" ><Text>Already registered? Login Here!</Text></Box>
       </Stack>
     )
   }
@@ -53,7 +124,7 @@ const Register = () => {
       <>
         <ChakraProvider>
           <Backdrop>
-            <LoginForm/>
+            <RegisterForm/>
           </Backdrop>
         </ChakraProvider>
       </>

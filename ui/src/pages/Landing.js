af5338e238ed390c44b02f1ec3c7b50453d53688
Edwin <ecl24@njit.edu>
2024-03-24 15:52:31 -0400

    Landing is presentable, global font added

--- ui/src/pages/Landing.js
+++ ui/src/pages/Landing.js
@@ -1,30 +1,28 @@
-import { Box, Button, Flex, Link } from '@chakra-ui/react';
+import { Text, Box, Button, Flex, Link } from '@chakra-ui/react';
 import { Link as RouterLink } from 'react-router-dom';
 import Hero from '../img/hero.png';
 
 const Landing = () => {
   return (
-    <Box>
+    <Box bg="#f8eede">
       {/* Navigation bar */}
-      <Flex justify="space-between" alignItems="center" p={4} bg="blue.500" color="white">
+      <Flex justify="flex-end" alignItems="center" p={5} bg="#353935" color="white">
         <Box>
-          <Link as={RouterLink} to="/">Home</Link>
-          <Link as={RouterLink} to="/about">About Us</Link>
-          <Link as={RouterLink} to="/contact">Contact</Link>
-          <Link as={RouterLink} to="/login">Login</Link>
+          <Link as={RouterLink} to="/" mr={4}>Home</Link>
+          <Link as={RouterLink} to="/about" mr={4}>About Us</Link>
+          <Link as={RouterLink} to="/contact" mr={4}>Contact</Link>
+          <Link as={RouterLink} to="/login" mr={4}>Login</Link>
         </Box>
       </Flex>
 
-      {/* Hero section */}
-      <Box bg="gray.200" p={8}>
-        {/* Add your hero image here */}
-        <Box textAlign="center">
-          <img src={Hero} alt="Hero Image" />
-          <h1>Welcome to Maple Frenzy</h1>
-          <p>You know why you're here. Waste no time, get to business.</p>
-          <Button colorScheme="blue" size="lg" mt={4} onClick={() => console.log("Get Started clicked")}>Get Started</Button>
+        {/* Hero Section */}
+        <Box>
+          <img src={Hero} alt="Hero"/>
+          <Box ml={8} mr={8} mt={8} bg="#353935" borderRadius="20" w="50%">
+            <Text fontSize={32} textShadow="1px 2px #000000" color="white" pt={4} pl={4}>You know why you're here. Waste no time, get to business.</Text>
+            <Button bg="#78A1BB" color="#353935" size="lg" mt={4} ml={8} mb={4} onClick={() => console.log("Get Started clicked")}>Get Started</Button>
+          </Box>
         </Box>
-      </Box>
     </Box>
   );
 }

05c3d55d8f5b451a2e13a852fcd3193f42450bf8
Edwin <ecl24@njit.edu>
2024-03-24 21:24:41 -0400

    Landing finalized for presentation, login and register routes functional

--- ui/src/pages/Landing.js
+++ ui/src/pages/Landing.js
@@ -4,23 +4,25 @@ import Hero from '../img/hero.png';
 
 const Landing = () => {
   return (
-    <Box bg="#f8eede">
+    <Box>
       {/* Navigation bar */}
       <Flex justify="flex-end" alignItems="center" p={5} bg="#353935" color="white">
-        <Box>
-          <Link as={RouterLink} to="/" mr={4}>Home</Link>
-          <Link as={RouterLink} to="/about" mr={4}>About Us</Link>
-          <Link as={RouterLink} to="/contact" mr={4}>Contact</Link>
-          <Link as={RouterLink} to="/login" mr={4}>Login</Link>
-        </Box>
+          <Link as={RouterLink} to="/" mr={6}>Home</Link>
+          <Link as={RouterLink} to="/about" mr={6}>About Us</Link>
+          <Link as={RouterLink} to="/contact" mr={6}>Contact</Link>
+          <Link as={RouterLink} to="/login" mr={4}>
+            <Button bg="#93d7bf" color="#353935" size="md" onClick={() => console.log("Login clicked")}>Login</Button>
+          </Link>
       </Flex>
 
         {/* Hero Section */}
-        <Box>
+        <Box bg="#F8EEDE" pb={100}>
           <img src={Hero} alt="Hero"/>
-          <Box ml={8} mr={8} mt={8} bg="#353935" borderRadius="20" w="50%">
-            <Text fontSize={32} textShadow="1px 2px #000000" color="white" pt={4} pl={4}>You know why you're here. Waste no time, get to business.</Text>
-            <Button bg="#78A1BB" color="#353935" size="lg" mt={4} ml={8} mb={4} onClick={() => console.log("Get Started clicked")}>Get Started</Button>
+          <Box ml={8} mr={8} mt={8} p={8} bg="#353935" borderRadius="20" w="50%">
+            <Text bg="#353935" fontSize={32} textShadow="1px 2px #000000" color="white" pt={4} pl={4}>With our platform, get right to business.</Text>
+            <Link as={RouterLink} to="/register" mr={4}>
+                <Button bg="#93d7bf" color="#353935" size="lg" mt={4} ml={8} mb={4} onClick={() => console.log("Get Started clicked")}>Get Started</Button>
+            </Link>
           </Box>
         </Box>
     </Box>

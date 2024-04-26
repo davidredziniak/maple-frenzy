4be9be0345a94ca724e35399ef46a541f6fd3521
davidredziniak <redziniakdavid@gmail.com>
2024-04-26 15:10:41 -0400

    Add profile functionality

--- ui/src/pages/Profile.js
+++ ui/src/pages/Profile.js
@@ -1,124 +1,139 @@
-import { Text, Box, Button, Flex, Card, CardHeader, CardBody, CardFooter, Spacer, Center, Stack } from "@chakra-ui/react";
+import {
+  Text,
+  Box,
+  Button,
+  Flex,
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Spacer,
+  Center,
+  Stack,
+} from "@chakra-ui/react";
 import { Link as RouterLink } from "react-router-dom";
 import Navbar from "./Navbar";
 import { useContext, useState, useEffect } from "react";
 import { AuthContext } from "./AuthContext";
 import { InfoOutlineIcon } from "@chakra-ui/icons";
 import logo from "../maplefrenzylogo.svg";
-import Footer from './Footer';
+import Footer from "./Footer";
 
-const ProfileBox = () => {
-  
-  
-  const { username, tradeCount, reputation, createdAt, lastLoggedIn } = useContext(AuthContext);
-  // const { username } = useContext(AuthContext);
-  // let tradeCount='0';
-  // let reputation='0';
-  // let createdAt='2024-04-24T00:22:47.112Z'; 
-  // let lastLoggedIn='2024-04-24T00:24:40.177Z';
-  return (
-    <Box pt="14vh" flex="1" bg="#F8EEDE" rounded="md" boxShadow="base" color={"black"}>
-      <Center>
-      <Stack>
-        <Center>
-          <Text fontSize="40px">
-            {username}
-          </Text>
-        </Center>
-        <Card bg="#353935" color={"white"}>
-        <Center>
-            <Flex pt="5vh">
-              <Text fontSize="20px">
-                User Information
-              </Text>
-              <Spacer pl=".5vw"/>
-              <InfoOutlineIcon boxSize={5}/>
-            </Flex>
-        </Center>
-          <CardBody>
-            <Flex>
-              <Text >
-                Trade Count:
-              </Text>
-              <Spacer />
-              <Text >
-                {tradeCount}
-              </Text>
-            </Flex>
-            <Flex>
-              <Text>
-                Reputation:
-              </Text>
-              <Spacer />
-              <Text >
-                {reputation}
-              </Text>
-            </Flex>
-            <Flex>
-              <Text>
-                Created At:
-              </Text>
-              <Spacer />
-              <Text >
-                {createdAt}
-              </Text>
-            </Flex>
-            <Flex>
-            <Text>
-              Last Log In:
-            </Text>
-            <Spacer />
-            <Text >
-              {lastLoggedIn}
-            </Text>
-          </Flex>
-          </CardBody>
-        </Card>
-        </Stack>
-      </Center>
-      
-    </Box>
-  );
-};
+function Profile(props) {
+  const { accessToken, refreshToken, userId } = useContext(AuthContext);
 
-const Profile = () => {
-  const { accessToken, userId, updateCreatedAt, updateLLI, updateReputation, updateTradeCount } = useContext(AuthContext);
-  const getData = async() => {
-    const response = await fetch(`https://maple-frenzy.onrender.com/api/user/${userId}`, {
-    method: "GET",
-    headers: {
-        "Content-Type": "application/json",
-        "x-access-token": `${accessToken}`
-    },
-    });
-    const data = await response.json();
-    if (response.status === 200) {
-    updateTradeCount(data.tradeCount);
-    updateReputation(data.reputation);
-    updateCreatedAt(new Date(data.createdAt).toDateString());
-    updateLLI(new Date(data.lastLoggedIn).toDateString());
+  const [username, setUsername] = useState("");
+  const [reputation, setReputation] = useState("");
+  const [tradeCount, setTradeCount] = useState("");
+  const [createdAt, setCreatedAt] = useState("");
+  const [lastLoggedIn, setLastLoggedIn] = useState("");
+
+  const getLocalTime = (time) => {
+    var date = new Date(time).toLocaleString();
+    return date.toString();
+  };
+
+  function configureState(data) {
+    if (!data.error) {
+      setUsername(data.username);
+      setReputation(data.reputation);
+      setTradeCount(data.tradeCount);
+      setCreatedAt(getLocalTime(data.createdAt));
+      setLastLoggedIn(getLocalTime(data.lastLoggedIn));
     }
   }
+
   useEffect(() => {
-    getData();
-  }, []); 
+    if (accessToken !== null && accessToken.length !== 0) {
+      getUserInfo(userId).then((data) => configureState(data));
+    }
+  }, [accessToken, userId]);
+
+  const getUserInfo = async (userId) => {
+    return fetch("https://maple-frenzy.onrender.com/api/user/" + userId, {
+      method: "GET",
+      headers: {
+        "x-access-token": accessToken,
+        "Content-Type": "application/json",
+      },
+    }).then((response) => response.json());
+  };
+
   return (
-    <Box >
+    <Box>
       {/* Navigation Bar */}
       <Navbar />
 
       {/* Content Section */}
-      <Flex h="100vh"  textColor="white">
-        
-        <ProfileBox size="250px"/>
+      <Flex h="100vh" textColor="white">
+        <ProfileBox
+          size="250px"
+          username={username}
+          reputation={reputation}
+          tradeCount={tradeCount}
+          createdAt={createdAt}
+          lastLoggedIn={lastLoggedIn}
+        />
       </Flex>
-      <Footer/>
+      <Footer />
+    </Box>
+  );
+}
+
+const ProfileBox = (props) => {
+  return (
+    <Box
+      pt="14vh"
+      flex="1"
+      bg="#F8EEDE"
+      rounded="md"
+      boxShadow="base"
+      color={"black"}
+    >
+      <Center>
+        <Stack>
+          <Center>
+            <Text fontSize="40px">{props.username}</Text>
+          </Center>
+          <Card bg="#353935" color={"white"}>
+            <Center>
+              <Flex pt="5vh">
+                <Text fontSize="20px">User Information</Text>
+                <Spacer pl=".5vw" />
+                <InfoOutlineIcon boxSize={5} />
+              </Flex>
+            </Center>
+            <CardBody>
+              <Flex>
+                <Text>Trade Count:</Text>
+                <Spacer />
+                <Text>{props.tradeCount}</Text>
+              </Flex>
+              <Flex>
+                <Text>Reputation:</Text>
+                <Spacer />
+                <Text>{props.reputation}</Text>
+              </Flex>
+              <Flex>
+                <Text>Created At:</Text>
+                <Spacer />
+                <Text>{props.createdAt}</Text>
+              </Flex>
+              <Flex>
+                <Text>Last Login:</Text>
+                <Spacer />
+                <Text>{props.lastLoggedIn}</Text>
+              </Flex>
+            </CardBody>
+          </Card>
+        </Stack>
+      </Center>
     </Box>
   );
 };
 
 export default Profile;
-  // let tradeCount='0';
-  // let reputation='0';
-  // let createdAt='2024-04-24T00:22:47.112Z'; 
-  // let lastLoggedIn='2024-04-24T00:24:40.177Z';
+// let tradeCount='0';
+// let reputation='0';
+// let createdAt='2024-04-24T00:22:47.112Z';
+// let lastLoggedIn='2024-04-24T00:24:40.177Z';
